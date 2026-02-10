e2af381a4827b0ed5361370c59f6bc7fb25b5381
1quiprodest <quiprodestt.t@gmail.com>
2026-02-10 01:57:39 +0100

    fix: types and imports for vercel

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Эти данные возьми в Supabase: Settings -> API
+// ИСПОЛЬЗУЙ import.meta.env, А НЕ process.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("ОШИБКА: Переменные окружения не найдены!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
