94192015a562dc950652e0d2e6895b3c7e137b4f
1quiprodest <quiprodestt.t@gmail.com>
2026-02-10 01:45:37 +0100

    fix: types and imports for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "tailwindcss";
 import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-// Манифест — это JSON с инфой о твоем проекте. 
-// Пока можно использовать этот тестовый URL:
-const manifestUrl = 'https://3b30-178-85-56-239.ngrok-free.app/tonconnect-manifest.json';
+// ТЕПЕРЬ ТУТ ССЫЛКА НА VERCEL, А НЕ NGROK
+const manifestUrl = 'insight-markets-nnu5-git-main-1quiprodests-projects.vercel.app';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider manifestUrl={manifestUrl}>
