a718e6aacc3501b88efa1526ba6142b74ff27ebb
1quiprodest <quiprodestt.t@gmail.com>
2026-02-11 14:48:42 +0100

    fix: types and imports for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = import.meta.env.MODE === 'development' 
-  ? `${window.location.origin}/tonconnect-manifest.json`
-  : '/tonconnect-manifest.json';
+const manifestUrl = 'https://insightmarkets.live/tonconnect-manifest.json';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider manifestUrl={manifestUrl}>
